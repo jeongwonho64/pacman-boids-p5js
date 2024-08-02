6176713377586aa38a9fc9ed3ba24ae58ebf2bf0
jeongwonho64 <attemptn63@gmail.com>
2024-08-02 20:08:57 +0800

    updating boid controls
    added boid controls and path following

--- boid.js
+++ boid.js
@@ -38,4 +38,110 @@ class Boid {
         endShape(CLOSE);
         pop();
     }
+    control(keymap) {
+        let toggle = false;
+        if (keymap["w"]) {
+            this.speed += 0.02;
+            toggle = true;
+        }
+        if (keymap["s"]) {
+            this.speed -= 0.02;
+            toggle = true;
+        }
+        if (keymap["a"]) {
+            this.rotation -= 0.04;
+            toggle = true;
+        }
+        if (keymap["d"]) {
+            this.rotation += 0.04;
+            toggle = true;
+        }
+
+        if (keyIsPressed && key == "w") {
+            //console.log(3)
+        }
+        if (keyIsPressed && key == "s") {
+            //this.speed -= 0.02;
+        }
+        if (keyIsPressed && key == "a") {
+            //this.rotation -= 0.04;
+            //console.log(33)
+        }
+        if (keyIsPressed && key == "d") {
+            //this.rotation += 0.04;
+        }
+        if (keyIsPressed && key == "d") {
+            //this.pos.x += 1;
+        } else {
+        }
+        if (abs(this.speed) > 0.01 && toggle == false) {
+            this.speed *= 0.965;
+            // if (this.speed >0){
+            //   this.speed -= 0.01;
+            // } else {
+            //   this.speed += 0.01;
+            // }
+        }
+        if (abs(this.speed) > this.maxSpeed) {
+            if (this.speed > 0) {
+                this.speed = this.maxSpeed;
+            } else {
+                this.speed = -this.maxSpeed;
+            }
+        }
+    }
+    seek(boid, arrival = false) {
+        //extrapolation?
+        let target = boid.position;
+        let vel = p5.Vector.sub(target, this.prevpos);
+        vel = p5.Vector.sub(target, target.velocity);
+        //console.log(vel.x, vel.y)
+        let tempvel = createVector(boid.velocity.x, boid.velocity.y);
+        tempvel.mult(5);
+        let newtarget = p5.Vector.add(tempvel, target);
+        let desired = p5.Vector.sub(newtarget, this.position); // A vector pointing from the location to the target
+        let desiredspeed = this.maxspeed;
+
+        // Scale to maximum speed
+        desired.setMag(desiredspeed);
+
+        // Steering = Desired minus velocity
+
+        let steer = p5.Vector.sub(desired, this.velocity);
+        steer.limit(this.maxforce); // Limit to maximum steering force
+        if (arrival) {
+            let slowRadius = 100;
+            let distance = steer.mag();
+            if (distance < slowRadius) {
+                desiredspeed = map(distance, 0, slowRadius, 0, this.maxSpeed);
+            }
+        }
+        this.applyForce(steer);
+        //this.prevpos = createVector(this.position.x, this.position.y);
+        return steer;
+    }
+    followpath(path) {
+        // Path following algorithm here!!
+
+        // Step 1 calculate future position
+        let future = this.velocity.copy();
+        future.mult(20);
+        future.add(this.position);
+        fill(255, 0, 0);
+        noStroke();
+        circle(future.x, future.y, 16);
+
+        // Step 2 Is future on path?
+        let target = findProjection(path.start, future, path.end);
+        fill(0, 255, 0);
+        noStroke();
+        circle(target.x, target.y, 16);
+
+        let d = p5.Vector.dist(future, target);
+        if (d > path.radius) {
+            return this.seek(target);
+        } else {
+            return createVector(0, 0);
+        }
+    }
 }

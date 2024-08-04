021161ac17075b48319fefefadaadc99de55b04e
jeongwonho64 <attemptn63@gmail.com>
2024-08-04 14:45:06 +0800

    finished

--- sketch.js
+++ sketch.js
@@ -120,4 +120,154 @@ class Vehicle {
         force.limit(this.maxForce);
         return force;
     }
+    arrive(target) {
+        // A vector pointing from the location to the target
+        let desired = p5.Vector.sub(target, this.position);
+        let d = desired.mag();
+        // Scale with arbitrary damping within 100 pixels
+        if (d < 100) {
+            var m = map(d, 0, 100, 0, this.maxspeed);
+            desired.setMag(m);
+        } else {
+            desired.setMag(this.maxspeed);
+        }
+
+        // Steering = Desired minus Velocity
+        let steer = p5.Vector.sub(desired, this.velocity);
+        steer.limit(this.maxforce); // Limit to maximum steering force
+        this.applyForce(steer);
+    }
+
+    show() {
+        // Draw a triangle rotated in the direction of velocity
+        let theta = this.velocity.heading() + PI / 2;
+        fill(127);
+        stroke(0);
+        strokeWeight(2);
+        push();
+        translate(this.position.x, this.position.y);
+        rotate(theta);
+        beginShape();
+        vertex(0, -this.r * 2);
+        vertex(-this.r, this.r * 2);
+        vertex(this.r, this.r * 2);
+        endShape(CLOSE);
+        pop();
+    }
+
+    edges() {
+        if (this.position.x > width + this.r) {
+            this.position.x = -this.r;
+        } else if (this.position.x < -this.r) {
+            this.position.x = width + this.r;
+        }
+        if (this.position.y > height + this.r) {
+            this.position.y = -this.r;
+        } else if (this.position.y < -this.r) {
+            this.position.y = height + this.r;
+        }
+    }
 }
+
+const flock = [];
+const preds = [];
+let control = [];
+let keyMap = {};
+
+function keyPressed() {
+    if (key in keyMap) {
+        // Check if the pressed key is one we care about
+        keyMap[key] = true; // Update its state to true
+    }
+}
+
+// Function to handle key release events
+function keyReleased() {
+    if (key in keyMap) {
+        // Check if the released key is one we care about
+        keyMap[key] = false; // Update its state to false
+    }
+}
+let wall1;
+let maze;
+let points = 0;
+function setup() {
+    createCanvas(800, 600);
+    keyMap["w"] = false;
+    keyMap["a"] = false;
+    keyMap["s"] = false;
+    keyMap["d"] = false;
+
+    //wall1 = new Wall(100,200,100,500);
+    maze = new Maze(50);
+    for (let i = 0; i < 30; i++) {
+        flock.push(new Boid("boid"));
+    }
+    for (let i = 0; i < 3; i++) {
+        preds.push(new Boid("pred"));
+        preds[i].maxSpeed = 1.0;
+    }
+    control.push(new Boid("pred"));
+    control[0].maxSpeed = 1.5;
+}
+
+function draw() {
+    background(51);
+    //console.log(maze.walls)
+    //console.log(flock)
+    for (let i = 0; i < flock.length; i++) {
+        let boid = flock[i];
+        let collide = boid.flock(flock, control, maze.walls);
+        if (collide) {
+            flock.splice(i, 1);
+            points++;
+            console.log(points);
+        } else {
+            boid.update(true);
+            //boid.show();
+        }
+    }
+
+    for (let i = 0; i < preds.length; i++) {
+        let boid = preds[i];
+        let collide = boid.flock(control, preds, maze.walls);
+
+        boid.update(true);
+        //boid.show();
+        if (collide) {
+            preds.splice(i, 1);
+            points -= 3;
+            console.log(points);
+        }
+    }
+    if (preds.length < 3) {
+        let chance = random(100);
+        if (chance < 4) {
+            preds.push(new Boid("pred"));
+            preds[preds.length - 1].maxSpeed = 1.0;
+        }
+    }
+    if (preds.length < 3) {
+        let chance = random(100);
+        if (chance < 15) {
+            flock.push(new Boid("pred"));
+            //flock[flock.length - 1].maxSpeed = 1.0;
+        }
+    }
+    for (const boid of control) {
+        boid.update(false, maze.walls);
+        boid.show22();
+        boid.flock(flock, control, maze.walls, true);
+        boid.control(keyMap);
+    }
+    if (frameCount == 60 * 60) {
+        createP(points);
+        noLoop();
+    }
+    push();
+    fill(255);
+    textSize(32);
+    text(60 - floor(frameCount / 60), 10, 40);
+    pop();
+}
+
